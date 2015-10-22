3e09eabb8743b1414fd1be2cbdcb296419cac299
Caitlynn <chow.tian.oo@gmail.com>
2015-10-23 01:31:20 +1100

    start LRU and now add pages to frame successfully

--- page-replace.py
+++ page-replace.py
@@ -6,9 +6,34 @@ from itertools import cycle
 
 pages = pagerefgen.generate(int(sys.argv[1]),int(sys.argv[2]))
 print(pages)
-def lru():
-    return
+class lru(doublyLinkedList.LinkedList):
+    def __init__(self, nframe):
+        self.nframe = nframe
+        self.npage = len(pages)
 
+    def createPage(self):
+        #pages = []
+        #pages = pagerefgen.generate(int(sys.argv[1]), int(sys.argv[[2]]))
+        #take the arguments to generate page reference list
+        mypage = doublyLinkedList.LinkedList()
+
+        if len(pages) >= self.nframe:
+            for i in range (self.nframe):
+                mypage.add(i, pages[i]) #add nframe number of pages to the frame
+        else:
+            for i in range (self.npage):
+                mypage.add(i, pages[i]) #add npage number of pages to the frame if there's less pages than frames
+        print(mypage.get_size())
+'''
+        for i in range (self.npage):
+            pagedict = {pages[i] : mypage._getNode(i).data}
+        print(pagedict)
+'''
+
+if __name__ == "__main__":
+    alg1 = lru(5)
+    alg1.createPage()
+'''
 def clock():
     pagedict = dict()
     pagedict[page] = index
@@ -19,3 +44,4 @@ def clock():
     for item in clockArray:
         return
     return
+'''
