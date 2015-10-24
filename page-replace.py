a1518c52da00277fc7983c58eda09d840923a503
Caitlynn <chow.tian.oo@gmail.com>
2015-10-24 14:11:18 +1100

    buggy LRU, need to fix the work with first N frame page numbers

--- page-replace.py
+++ page-replace.py
@@ -4,35 +4,53 @@ import pagerefgen
 import doublyLinkedList
 from itertools import cycle
 
+        #take the arguments to generate page reference list
 pages = pagerefgen.generate(int(sys.argv[1]),int(sys.argv[2]))
 print(pages)
 class lru(doublyLinkedList.LinkedList):
     def __init__(self, nframe):
         self.nframe = nframe
-        self.npage = len(pages)
 
     def createPage(self):
-        #pages = []
-        #pages = pagerefgen.generate(int(sys.argv[1]), int(sys.argv[[2]]))
-        #take the arguments to generate page reference list
         mypage = doublyLinkedList.LinkedList()
+        for i in range (self.nframe):
+            mypage.add(100,pages.pop(0))  #add the ith element in pages list to the end of the linked list
+        for i in range (mypage.get_size()):
+            print(mypage._getNode(i).data)
 
-        if len(pages) >= self.nframe:
-            for i in range (self.nframe):
-                mypage.add(i, pages[i]) #add nframe number of pages to the frame
-        else:
-            for i in range (self.npage):
-                mypage.add(i, pages[i]) #add npage number of pages to the frame if there's less pages than frames
-        print(mypage.get_size())
-'''
-        for i in range (self.npage):
-            pagedict = {pages[i] : mypage._getNode(i).data}
-        print(pagedict)
-'''
+        for i in range (len(pages)):
+            if mypage.get_size() == 0:
+                mypage.add(pages[i])
+            else:
+                nodeindex = self.checkNode(mypage, pages[i])
+                if nodeindex:
+                    self.swap(mypage, nodeindex)
+                else:
+                    self.pagefault(mypage, pages[i])
+
+
+    def checkNode(self,frames, node):
+        nodeindex = frames.fine(node)
+        if nodeindex:
+            return nodeindex
+        return False
+
+    def swap(self,frames,nodeindex):
+        tmp = frames._getNode(nodeindex) #tmp = the node we found
+        frames.remove(nodeindex) #delete the node from the linkedlist
+        frames.add(100,tmp) #then add it to the end of the list
+        return frames
+
+    def pagefault(self, mypage, node):
+        mypage.remove(0) #remove the first element of the list
+        mypage.add(100,node) #add the node to the end of the frame
+        return mypage
+
 
 if __name__ == "__main__":
     alg1 = lru(5)
     alg1.createPage()
+
 '''
 def clock():
     pagedict = dict()
