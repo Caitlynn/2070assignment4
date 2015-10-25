f572e0b600ed352c2b3224fcce2a9dc3216a83fb
Caitlynn <chow.tian.oo@gmail.com>
2015-10-25 21:08:09 +1100

    fixed some small bugs of clock and gave up on LRU.

--- doublyLinkedList.py
+++ doublyLinkedList.py
@@ -1,36 +1,105 @@
-__author__ = 'Caitlynn'
+'''
+Created on 19 Oct 2015
+@:author: Caitlynn Zhou
+'''
+
+'''
+class Node is for creating a node and setting the next node and previous node, and also it can get data of the input node
+'''
 class Node(object):
+    '''
+        Initialization function for class Node
+        @:param: a node
+        @:return: none
+    '''
     def __init__(self, d, n = None, p = None):
         self.data = d
         self.next_node = n
         self.prev_node = p
-
+    '''
+    get next function returns the next node of the current node
+    @:param: none
+    @:return: next node
+    '''
     def get_next(self):
         return self.next_node
+    '''
+    set_next function set the input node as next node of the current node
+    @:param: n which is the next node
+    @:return: none
+    '''
     def set_next(self,n):
         self.next_node = n
+    '''
+    get next function returns the previous node of the input node
+    @:param: none
+    @:return: previous node
+    '''
     def get_prev(self):
         return self.prev_node
+    '''
+    set_prev function set the input node as previous node of the current node
+    @:param: p which is the previous node
+    @:return: none
+    '''
     def set_prev(self,p):
         self.prev_node = p
+    '''
+    get data function returns the data of the current node
+    @:param: none
+    @:return: the data of the current node
+    '''
     def get_data(self):
         return self.data
+    '''
+    set_data function set the input data to the current node
+    @:param: d which is the data that's going to be set to the current node
+    @:return: none
+    '''
     def set_data(self,d):
         self.data = d
 
+'''
+class LinkedList is a doubly linked list structure, it has appending function, check empty function,get size function, deletion function and find node function
+'''
 class LinkedList(object):
+    '''
+        Initialization function for class LinkedList
+        @:param: root of the linkedlist set to none
+        @:return: none
+    '''
     def __init__(self, r = None):
         self.root = r
         self.size = 0
+    '''
+    get size function returns the size of the linked list
+    @:param: none
+    @:return: the size of the linked list
+    '''
     def get_size(self):
         return self.size
+    '''
+    is_empty function returns a boolean if the linked list is empty
+    @:param: none
+    @:return: a boolean to show if the linked list is empty
+    '''
     def is_empty(self):
         return self.size == 0
+    '''
+    get node function returns the node of the input index of the linked list
+    @:param: index of the node that user wants to get
+    @:return: the node of the input index
+    '''
     def _getNode(self,index):
         node = self.root
         for _ in range(index):
             node = node.get_next()
         return node
+    '''
+    add function add an item to index position in the linked list
+    @:param: index the position user want to add, item
+    @:return: none
+    '''
 
     def add(self,index, item):
         if index < 0:
@@ -44,6 +113,11 @@ class LinkedList(object):
             node = self._getNode(index-1)
             node.next_node = Node(item, node.next_node, node)
         self.size += 1
+    '''
+    remove function remove the input index node from the linked list by connect the previous node to the next node of the deletion node
+    @:param: index of the deletion node
+    @:return: none
+    '''
     def remove(self,index):
         if self.is_empty():
             raise IndexError("list is empty")
@@ -61,7 +135,12 @@ class LinkedList(object):
             if prev:
                 prev.set_next(next)
         self.size -= 1
+    '''
+    find function checks the input node in the linked list and return the index of the node
+    @:param: d -- a node
+    @:return: the index of the node if found, None is the node is not found.
 
+    '''
     def find(self,d):
         this_node = self.root
         i = 0
--- page-replace.py
+++ page-replace.py
@@ -1,15 +1,27 @@
-__author__ = 'Caitlynn'
+'''
+Created on 19 Oct 2015
+@:author: Caitlynn Zhou
+'''
+
 import sys
 import pagerefgen
 import doublyLinkedList
 
-#take the arguments to generate page reference list
+#take the arguments to generate page reference list as global variable
 pages = pagerefgen.generate(int(sys.argv[1]),int(sys.argv[2]))
-print(pages)
+print('page reference list: ' + str(pages))
+'''
+class LRU uses LRU algorithms to run referencing pages and replace pages if there is a page fault
+'''
 class lru(doublyLinkedList.LinkedList):
     def __init__(self, nframe):
         self.nframe = nframe
 
+    '''
+    the main function to reference pages and replace pages if there is a page fault
+    @:param: none
+    @:return: none
+    '''
     def referencePage(self):
         mypage = doublyLinkedList.LinkedList()
         printstring = ''
@@ -29,25 +41,44 @@ class lru(doublyLinkedList.LinkedList):
             for i in range (mypage.get_size()):
                 printstring += str(mypage._getNode(i).data)
                 print(printstring)
-
+    '''
+    this function checks if the node is in the frames
+    @:param: frames-- a linked list; node: the node that needs to be check in the linked list
+    @:return: the node index if found or False if not
+    '''
     def checkNode(self,frames, node):
         nodeindex = frames.find(node)
         if nodeindex:
             return nodeindex
         return False
-
+    '''
+    this function remove the page from the list then add it to the end of the list
+    @:param: frames: a linked list structure; nodeindex: the node that needs to move to the end of the list
+    @:return: frames
+    '''
     def swap(self,frames,nodeindex):
         tmp = frames._getNode(nodeindex) #tmp = the node we found
         frames.remove(nodeindex) #delete the node from the linkedlist
         frames.add(100,tmp) #then add it to the end of the list
         return frames
-
+    '''
+    this function removes the first element of the linked list then add the element to the last of the linked list
+    @:param: mypage: the linked list; node: the node that needs to be added to the end of the list
+    @:return: mypage
+    '''
     def pagefault(self, mypage, node):
         mypage.remove(0) #remove the first element of the list
         mypage.add(100,node) #add the node to the end of the frame
         return mypage
 
 class Clock():
+    '''
+    the initialisation function of class Clock, nframe is the number of frames, pagedict is a dictionaries that contains [page: index],
+    pointer is the pointer to present where in the list is referencing a page, clock list is a list of lists with nframes length and
+    has pages and flags in it, page fault is the counter of the page fault.
+    @:param: nframe
+    @:return: none
+    '''
     def __init__(self,nframe):
         self.nframe = nframe
         self.pagedict = {}  #a dictionary to hold page number and the index of the page number in the list or none if the page is not there
@@ -55,12 +86,17 @@ class Clock():
         self.clockList = []
         self.pagefault = 0
 
+    '''
+    this function runs to reference pages and replace page if there is a page fault
+    @:param: none
+    @:return: none
+    '''
     def referencePages(self):
         for i in range (self.nframe):
             self.clockList.append([-1,0]) # append [page,flag]
 
         for i in range (len(pages)):
-            if self.pointer >= self.nframe:
+            if self.pointer >= self.nframe: # set pointer back to 0 if it's bigger than nframe
                 self.pointer = 0
 
             if self.checkPage(pages[i]) == None: #page is not found
@@ -68,50 +104,66 @@ class Clock():
             else:
                 index = self.checkPage(pages[i])
                 self.clockList[index][1] = 1
-            print('page reference list: ' + str(pages))
+
             print('clock list: ' + str(self.clockList))
             print('current referecing page: ' + str(pages[i]))
-            print('page fault: ' + str(self.pagefault))
+        print('page fault: ' + str(self.pagefault))
 
 
+    '''
+    this function handles a pagefault
+    @:param: page -- referencing page
+    @:return: none
+    '''
     def processPageFault(self, page):
-        self.pagefault += 1
         if self.list_not_full(): #if the list is not full then add page to the pointer position
             self.clockList[self.pointer][0] = page
             self.clockList[self.pointer][1] = 1
-            self.pagedict[page] = self.pointer
+            self.pagedict[page] = self.pointer #add the new data to the dictionary
             self.pointer += 1
 
-        elif self.clockList[self.pointer][1] == 0:
-            self.pagedict[self.clockList[self.pointer][0]] = None
-            self.clockList[self.pointer][0] = page
-            self.pagedict[page] = self.pointer
-            self.clockList[self.pointer][1] = 1
+        elif self.clockList[self.pointer][1] == 0: # the list is full but the pointer position flag is 0
+            self.pagefault += 1
+            self.pagedict[self.clockList[self.pointer][0]] = None # delete the node from the dictionary
+            self.clockList[self.pointer][0] = page #set the new data
+            self.pagedict[page] = self.pointer #add new index to the dictionary
+            self.clockList[self.pointer][1] = 1 #flag = 1
             self.pointer += 1
 
-        elif self.clockList[self.pointer][1] == 1:
-            self.clockList[self.pointer][1] = 0
-            self.pointer += 1
-            self.pagedict[page] = None
+        elif self.clockList[self.pointer][1] == 1: #if the list is full and current flag is 1
+            self.clockList[self.pointer][1] = 0 #set the current flag to 0
+            self.pointer += 1 #move to next one
+            self.pagedict[page] = None # delete this one from the dictionary
 
-            while True:
-                if self.pointer >= len(self.clockList):
+            while True: #loop to find the flag 0
+                if self.pointer >= len(self.clockList): #reset pointer if it's bigger than the length of the clock list
                     self.pointer = 0
-                if self.clockList[self.pointer][1] == 0:
-                    self.pagedict[self.clockList[self.pointer][0]] = None
-                    self.clockList[self.pointer][0] = page
-                    self.pagedict[page] = self.pointer
-                    self.clockList[self.pointer][1] = 1
-                    return
-                elif self.clockList[self.pointer][1] == 1:
-                    self.pointer += 1
-
+                if self.clockList[self.pointer][1] == 0: # flag 0 found
+                    self.pagefault += 1
+                    self.pagedict[self.clockList[self.pointer][0]] = None # delete the current one from the dictionary
+                    self.clockList[self.pointer][0] = page #add the page to the list
+                    self.pagedict[page] = self.pointer #add the data to the dictionary
+                    self.clockList[self.pointer][1] = 1 #flag =1
+                    return #break
+                elif self.clockList[self.pointer][1] == 1: #flag is not found
+                    self.clockList[self.pointer][1] = 0
+                    self.pointer += 1 #move to next one
+    '''
+    this function returns a boolean of if the list if full -- checking if there is still -1 in the list
+    @:param: none
+    @:return: boolean representing if the list is full
+    '''
     def list_not_full(self):
         for i in range (len(self.clockList)):
             if self.clockList[i][0] == -1:
                 return True
         return False
 
+    '''
+    this function returns the result of checking the input in the dictionary
+    @:param: page
+    @:return: index if it's found, otherwise returns none
+    '''
     def checkPage(self, page):
         if self.pagedict.get(page):
             return self.pagedict.get(page)
